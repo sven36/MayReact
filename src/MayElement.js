b477eafe6035f8e4b83cf0334de6d361ed83990b
chengweiliang <chengweiliang@58ganji.com>
2017-11-08 19:32:36 +0800

    configing

--- src/MayElement.js
+++ src/MayElement.js
@@ -1,5 +1,25 @@
 
+export function createElement(type,props,children) {
 
-export function createElement(){
-    
+    var element = {
+
+    }
+    element.type = type;
+    extend(element.props, props)
+    if (arguments.length > 2) {
+        var args=[].slice.call(arguments);
+        while(){
+            
+        }
+    }else{
+        element.children=children;
+    }
+    return element;
+}
+
+function extend(target, src) {
+    for (var key in src) {
+        target[key] = src[key];
+    }
+
 }
