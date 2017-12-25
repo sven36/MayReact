a56bcc3b07eda7d07570d92958e2a447cbad3296
sven36 <sven36@163.com>
2017-12-26 00:11:29 +0800

    we

--- scripts/webp.js
+++ scripts/webp.js
@@ -1,22 +1,62 @@
 
-// const sharp=require('sharp');
-var path=require('path');
-var fs=require('fs');
-// sharp('/scripts/404.png')
-//   .resize(300, 200)
-//   .toFile('output.jpg', function(err) {
-//       console.log(err);
-//     // output.jpg is a 300 pixels wide and 200 pixels high image
-//     // containing a scaled and cropped version of input.jpg
-//   });
-  const http = require('http');
+const sharp = require('sharp');
+var path = require('path');
+var fs = require('fs');
+var _img = path.join(__dirname, './img/1.jpg');
 
-  const server = http.createServer((req, res) => {
-    fs.readFile(path.join(__dirname,'404.png'),'binary',(error,file)=>{
-      res.writeHead(200, {"Content-Type": "image/png"});
-      res.write(file, "binary");
-      res.end();
-    });
-  });
-  
-  server.listen(3333);
+var transformImg = function (_img) {
+	var _prevSize;
+	fs.readFile(_img, (err, file) => {
+		_prevSize = file.length;
+	})
+	sharp(_img)
+		.toFile(path.join(__dirname, './out-webp/png-compressed.webp'), function (err, file) {
+			if (err) throw err;
+
+			console.log('transformed');
+			var _prev = Math.round(_prevSize / 1024);
+			var _afer = Math.round(file.size / 1024);
+			console.log('prev  ' + _prev + 'KB');
+			console.log('--------------------');
+			console.log('webp  ' + _afer + 'KB');
+			console.log('resized    ' + Math.round((_prev - _afer) * 100 / _prev) + '%');
+		});
+}
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+	var isWebp = false;
+	if (req && req.headers.accept && req.headers.accept.indexOf('image/webp') > 0) {
+		isWebp = true;
+		console.log(req.accept);
+	};
+	var _path;
+	if (isWebp) {
+		_path = path.join(__dirname, '/out-webp/jpg.webp');
+	} else {
+		_path = path.join(__dirname, req.url);
+	}
+	fs.readFile(_path, (error, file) => {
+		if (isWebp) {
+			res.writeHead(200, { "Content-Type": "image/webp" });
+		} else {
+			res.writeHead(200, { "Content-Type": "image/webp" });
+		}
+		if (file) {
+			res.write(file);
+		}
+		res.end();
+	});
+});
+
+server.listen(3333);
+
+
+// var buffer= fs.readFile(_img,(err,data)=>{
+//   sharp(data).toFile('output.webp', function(err) {
+//     console.log(err);
+//   // output.jpg is a 300 pixels wide and 200 pixels high image
+//   // containing a scaled and cropped version of input.jpg
+// });
+// })
