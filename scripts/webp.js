01ae57e21313ecdcc6c4e0e5bc67825a94b52ab4
chengweiliang <chengweiliang@58ganji.com>
2017-12-25 19:32:54 +0800

    test

--- scripts/webp.js
+++ scripts/webp.js
@@ -1,6 +1,7 @@
 
-const sharp=require('sharp');
+// const sharp=require('sharp');
 var path=require('path');
+var fs=require('fs');
 // sharp('/scripts/404.png')
 //   .resize(300, 200)
 //   .toFile('output.jpg', function(err) {
@@ -11,31 +12,10 @@ var path=require('path');
   const http = require('http');
 
   const server = http.createServer((req, res) => {
-    // req 是 http.IncomingMessage 的实例，这是一个 Readable Stream
-    // res 是 http.ServerResponse 的实例，这是一个 Writable Stream
-  
-    let body = '';
-    // 接收数据为 utf8 字符串，
-    // 如果没有设置字符编码，将接收到 Buffer 对象。
-    req.setEncoding('utf8');
-  
-    // 如果监听了 'data' 事件，Readable streams 触发 'data' 事件 
-    req.on('data', (chunk) => {
-      body += chunk;
-    });
-  
-    // end 事件表明整个 body 都接收完毕了 
-    req.on('end', () => {
-      try {
-        const data = JSON.parse(body);
-        // 发送一些信息给用户
-        res.write(typeof data);
-        res.end();
-      } catch (er) {
-        // json 数据解析失败 
-        res.statusCode = 400;
-        return res.end(`error: ${er.message}`);
-      }
+    fs.readFile(path.join(__dirname,'404.png'),'binary',(error,file)=>{
+      res.writeHead(200, {"Content-Type": "image/png"});
+      res.write(file, "binary");
+      res.end();
     });
   });
   
