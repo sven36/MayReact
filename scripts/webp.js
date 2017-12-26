9e75dc8628e566a072fdc7479f8c967a8c624180
chengweiliang <chengweiliang@58ganji.com>
2017-12-26 19:40:43 +0800

    edit

--- scripts/webp.js
+++ scripts/webp.js
@@ -1,4 +1,3 @@
-
 const sharp = require('sharp');
 var path = require('path');
 var fs = require('fs');
@@ -8,20 +7,21 @@ var transformImg = function (_img) {
 	var _prevSize;
 	fs.readFile(_img, (err, file) => {
 		_prevSize = file.length;
-	})
-	sharp(_img)
-		.toFile(path.join(__dirname, './out-webp/png-compressed.webp'), function (err, file) {
-			if (err) throw err;
+		sharp(_img)
+			.toFile(path.join(__dirname, './out-webp/1.jpg.webp'), function (err, file) {
+				if (err) throw err;
 
-			console.log('transformed');
-			var _prev = Math.round(_prevSize / 1024);
-			var _afer = Math.round(file.size / 1024);
-			console.log('prev  ' + _prev + 'KB');
-			console.log('--------------------');
-			console.log('webp  ' + _afer + 'KB');
-			console.log('resized    ' + Math.round((_prev - _afer) * 100 / _prev) + '%');
-		});
+				console.log('--------------------');
+				console.log('transformed');
+				var _prev = Math.round(_prevSize / 1024);
+				var _afer = Math.round(file.size / 1024);
+				console.log('prev  ' + _prev + 'KB');
+				console.log('webp  ' + _afer + 'KB');
+				console.log('resized    ' + Math.round((_prev - _afer) * 100 / _prev) + '%');
+			});
+	})
 }
+// transformImg(_img);
 
 const http = require('http');
 
@@ -35,13 +35,17 @@ const server = http.createServer((req, res) => {
 	if (isWebp) {
 		_path = path.join(__dirname, '/out-webp/jpg.webp');
 	} else {
-		_path = path.join(__dirname, req.url);
+		_path = path.join(__dirname, req.url); //req.url
 	}
 	fs.readFile(_path, (error, file) => {
 		if (isWebp) {
-			res.writeHead(200, { "Content-Type": "image/webp" });
+			res.writeHead(200, {
+				"Content-Type": "image/webp"
+			});
 		} else {
-			res.writeHead(200, { "Content-Type": "image/webp" });
+			res.writeHead(200, {
+				"Content-Type": "image/webp"
+			});
 		}
 		if (file) {
 			res.write(file);
@@ -60,3 +64,12 @@ server.listen(3333);
 //   // containing a scaled and cropped version of input.jpg
 // });
 // })
+
+// function checkWebp() {
+//   try {
+//     return (document.createElement('canvas').toDataURL('image/webp').indexOf('data:image/webp') == 0);
+//   } catch (err) {
+//     return false;
+//   }
+// }
+// console.log(checkWebp()); // true or false
